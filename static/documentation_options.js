f535995f5215c48f64a734d92924fb33b3d2ff74
Jerome Pioux <jerome.pioux@atos.net>
2020-07-28 17:20:59 -0700

    Minor fixes for myqlm-0.0.7

--- static/documentation_options.js
+++ static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'myQLM-0.0.7.122',
+    VERSION: 'myQLM-0.0.7.19',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
