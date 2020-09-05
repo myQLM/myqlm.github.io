83e09359f2ee68a4f098b2e45a418e07ac07ac4b
Jerome Pioux <jerome.pioux@atos.net>
2020-09-04 17:59:43 -0700

    Matched initial pypi release (1.1.2)

--- static/documentation_options.js
+++ static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'myQLM-0.0.7.8',
+    VERSION: 'myQLM-X.X.X.12',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
