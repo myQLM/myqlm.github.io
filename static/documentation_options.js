a60592a4ffefb4f93d0941ad61e78504ce3d13d3
Jerome Pioux <jerome.pioux@atos.net>
2020-09-05 09:39:33 -0700

    Documentation for myQLM 1.1.5

--- static/documentation_options.js
+++ static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'myQLM-X.X.X.12',
+    VERSION: 'myQLM-1.1.5.20',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
